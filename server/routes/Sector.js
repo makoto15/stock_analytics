1a6b557f183eb4b71cb2db646f92f921f41f13a1
白石誠 <makotinator.15@gmail.com>
2021-06-18 00:45:36 +0900

    modified error

--- server/routes/Sector.js
+++ server/routes/Sector.js
@@ -18,38 +18,45 @@ router.get('/', async (req, res) => {
  * セクターIDから該当するセクターの上位10株の情報を返す
  */
 router.get('/:sectorId/:sectorName', async (req, res) => {
-    let stockList = await Stock.find({"sector_id":req.params.sectorId});
-    const stockNumInSector = stockList.length;
-    if (stockNumInSector > 10) {
+    try {
+      let stockList = await Stock.find({"sector_id":req.params.sectorId}); 
+      const stockNumInSector = stockList.length;
+      if (stockNumInSector > 10) {
         stockList = stockList.slice(0, 10)
-    }
+      }
 
-    const finalResponse = {}
-    for (let index = 0; index < stockList.length; index++) {
-        await Promise.all([
+      let finalResponse = {}
+      for (let index = 0; index < stockList.length; index++) {
+        try{
+          await Promise.all([
             getStockData.getStockQuote(stockList[index]["symbol"]),
             getStockData.getStockLogo(stockList[index]["symbol"]),
         ])
-            .then(function (results) {
-                //close(終値)とpreviousClose(前日終値)の差分を追加
-                let addedQuote = Object.assign(results[0], {close_previousClose_diff :results[0]['close'] - results[0]['previousClose']})
-                let quoteAndLogo = Object.assign(addedQuote, results[1])
-                const returnKeys = [
-                    'symbol',
-                    'url',
-                    'companyName',
-                    'close_previousClose_diff',
-                ];
-                const responseData = {}
-                Object.keys(quoteAndLogo).map(function (key) {
-                    if (returnKeys.indexOf(key) >= 0) {
-                        responseData[key] = quoteAndLogo[key]
-                    }
-                })
-                finalResponse[quoteAndLogo["companyName"]] = responseData
-            });
+          .then(function (results) {
+            //close(終値)とpreviousClose(前日終値)の差分を追加
+              let addedQuote = Object.assign(results[0], {close_previousClose_diff :results[0]['close'] - results[0]['previousClose']})
+              let quoteAndLogo = Object.assign(addedQuote, results[1])
+              const returnKeys = [
+                'symbol',
+                'url',
+                'companyName',
+                'close_previousClose_diff',
+              ];
+              const responseData = {}
+              for (let index = 0;index<returnKeys.length; index++) {
+                responseData[returnKeys[index]] = quoteAndLogo[returnKeys[index]]
+              };
+              finalResponse[responseData["companyName"]] = responseData
+            })
+        } catch(err) {
+          res.status(400).send("error has occured in for block");
+        }
+
+      }
+      res.json(JSON.stringify(finalResponse))
+    } catch(err) {
+      res.status(400).send("error has occured");
     }
-    res.json(JSON.stringify(finalResponse))
 })
 
 
--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 
+fetch('/api/sector/0001/consumer-defensive').then(response => {
+  console.log(response.json());
+})
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
