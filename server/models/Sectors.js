ef3e37d2bd1ec623fd116b2f7cdb1c9d85839d16
kairi Yasumatsu <kairi.matibuse@gmail.com>
2021-06-19 14:25:38 +0900

    modify Api res

--- server/models/Sectors.js
+++ server/models/Sectors.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const SectorSchema = mongoose.Schema({
     sector_id:Number,
     name_en:String,
-    name_jp:Number
+    name_jp:String
 })
 
 module.exports = mongoose.model('Sector', SectorSchema);
